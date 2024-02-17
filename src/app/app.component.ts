a10012fe5a7231275324fb4c2d866cc3088b1257
ismael <ismaelreyplata@gmail.com>
2024-02-17 21:45:03 +0100

    corregido wrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { MarsComponent } from './mars/mars.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent, MarsComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, MarsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
