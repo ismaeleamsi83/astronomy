742f99e8020fd28d9504c655bc12e85188df52bb
ismael <ismaelreyplata@gmail.com>
2024-02-20 19:55:40 +0100

    menu con iconos ok

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -15,9 +15,9 @@ export class HeaderComponent {
   @ViewChild('menuShow') menuShow!: ElementRef;
 
   menu = [
-    { 'id': 0, 'name': 'Home', 'url': '', 'selected': false},
-    { 'id': 1, 'name': 'Asteroids', 'url':'asteroids', 'selected': false },
-    { 'id': 2, 'name': 'Mars', 'url':'mars', 'selected': false },
+    { 'id': 0, 'icon': '../../assets/images/home.png','name': 'Home', 'url': '', 'selected': false},
+    { 'id': 1, 'icon': '../../assets/images/asteroid.png','name': 'Asteroids', 'url':'asteroids', 'selected': false },
+    { 'id': 2, 'icon': '../../assets/images/mars.png','name': 'Mars', 'url':'mars', 'selected': false },
   ];
 
   toggleActive(index: number){
@@ -33,14 +33,18 @@ export class HeaderComponent {
   }
 
   showMenu(){
-    console.log("pulsado");
-    if(this.menuShow.nativeElement.classList.contains('menu')){
-      this.menuShow.nativeElement.classList.add('showMenu');
-      this.menuShow.nativeElement.classList.remove('menu');
-    }else{
-      this.menuShow.nativeElement.classList.add('menu');
-      this.menuShow.nativeElement.classList.remove('showMenu');
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth < 769) {
+      if(this.menuShow.nativeElement.classList.contains('menu')){
+        this.menuShow.nativeElement.classList.add('showMenu');
+        this.menuShow.nativeElement.classList.remove('menu');
+      }else{
+        this.menuShow.nativeElement.classList.add('menu');
+        this.menuShow.nativeElement.classList.remove('showMenu');
+      }
     }
+    
     
   }
 
